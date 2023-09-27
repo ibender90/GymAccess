8a22a6d7579a652d1197217dc62afa9031ff7b29
Artjom_Stalev <stalev.artjom@gmail.com>
2023-09-27 15:29:21 +0300

    [user CRUD operations] minor refactoring

--- src/main/java/ru/geekbrains/gym/mapper/PaidPeriodMapper.java
+++ src/main/java/ru/geekbrains/gym/mapper/PaidPeriodMapper.java
@@ -3,6 +3,7 @@ package ru.geekbrains.gym.mapper;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
+import ru.geekbrains.gym.constant.Constant;
 import ru.geekbrains.gym.dto.PaidPeriodDto;
 import ru.geekbrains.gym.model.PaidPeriod;
 
@@ -11,13 +12,13 @@ import ru.geekbrains.gym.model.PaidPeriod;
 public interface PaidPeriodMapper extends EntityMapper<PaidPeriod, PaidPeriodDto>{
 
     @Mapping(source = "entity.id", target = "id")
-    @Mapping(source = "entity.dateTo", target = "dateTo", dateFormat = "dd-MM-yyyy HH:mm:ss")
-    @Mapping(source = "entity.dateFrom", target = "dateFrom", dateFormat = "dd-MM-yyyy HH:mm:ss")
+    @Mapping(source = "entity.dateTo", target = "dateTo", dateFormat = Constant.DATE_FORMAT)
+    @Mapping(source = "entity.dateFrom", target = "dateFrom", dateFormat = Constant.DATE_FORMAT)
     PaidPeriodDto toDto(PaidPeriod entity);
 
     @Mapping(source = "dto.id", target = "id")
-    @Mapping(source = "dto.dateTo", target = "dateTo", dateFormat = "dd-MM-yyyy HH:mm:ss")
-    @Mapping(source = "dto.dateFrom", target = "dateFrom", dateFormat = "dd-MM-yyyy HH:mm:ss")
+    @Mapping(source = "dto.dateTo", target = "dateTo", dateFormat = Constant.DATE_FORMAT)
+    @Mapping(source = "dto.dateFrom", target = "dateFrom", dateFormat = Constant.DATE_FORMAT)
     PaidPeriod toEntity(PaidPeriodDto dto);
 
 
